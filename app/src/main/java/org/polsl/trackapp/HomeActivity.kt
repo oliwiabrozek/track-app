bc13748971bc23627364c72ca432268b1018ffce
oliwiabrozek <oliwiabrozek@gmail.com>
2020-05-10 17:27:14 +0200

    Overflow item - Sign Out

--- app/src/main/java/org/polsl/trackapp/HomeActivity.kt
+++ app/src/main/java/org/polsl/trackapp/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -13,14 +15,14 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        setupUI()
+        //setupUI()
     }
 
-    private fun setupUI() {
-        sign_out_button.setOnClickListener {
-            signOut()
-        }
-    }
+//    private fun setupUI() {
+//        sign_out_button.setOnClickListener {
+//            signOut()
+//        }
+//    }
 
     private fun signOut() {
         startActivity(SignInActivity.getLaunchIntent(this))
@@ -30,6 +32,22 @@ class HomeActivity : AppCompatActivity() {
     companion object {
         fun getLaunchIntent(from: Context) = Intent(from, HomeActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.overflow_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { // Handle item selection
+        return when (item.getItemId()) {
+            R.id.sign_out_menu_item -> {
+                signOut()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
